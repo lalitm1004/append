d4027b971fb4ca80f123abf410a7cf7d22aa83d7
Rachit Kumar <rachit.kumar@runwayave.com>
2026-05-12 21:07:39 +0530

    improve flow

--- v1/main.py
+++ v1/main.py
@@ -9,6 +9,7 @@ class Animal:
     def __init__(self, species):
         conn = sqlite3.connect("database.db")
         table_name = f"talbe_{uuid.uuid4().hex}"
+        self.__del__()
 
     def __del__() -> None:
         conn.execute(f"DROP TABLE IF EXISTS {table_name}")

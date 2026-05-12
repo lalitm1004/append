7dcf67a26fdae88af9aae3634996d0d9c7b5ac69
Rachit Kumar <rachit.kumar@runwayave.com>
2026-05-12 19:57:31 +0530

    prevent redundancies

--- v1/main.py
+++ v1/main.py
@@ -6,3 +6,4 @@ class Animal:
     def __init__(self, species):
         conn = sqlite3.connect("database.db")
         table_name = f"talbe_{uuid.uuid4().hex}"
+        conn.execute(f"DROP TABLE IF EXISTS {table_name}")
